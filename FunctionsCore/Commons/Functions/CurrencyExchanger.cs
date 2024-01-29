24dba4d1a52e8ce6426c58a899ea725835590a27
Béla Kellner <bela.kellner@icloud.com>
2024-01-29 17:22:28 +0100

    Added Currencies
    - Added Currency Enums
    - Added Currency Exchanger (right now only containing currency symbols, need to add FreeCurrencyAPI -> Memory and then use that memory data to exchange)

---
+++ FunctionsCore/Commons/Functions/CurrencyExchanger.cs
@@ -0,0 +1,59 @@
+﻿using FunctionsCore.Enums.Unit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionsCore.Commons.Functions
+{
+    /// <summary>
+    /// Currency Exchanger (using FreeCurrencyAPI)
+    /// </summary>
+    public static class CurrencyExchanger
+    {
+        private static readonly Dictionary<CurrencyUnit, string> Units = new Dictionary<CurrencyUnit, string>
+        {
+            { CurrencyUnit.EUR, "€" },
+            { CurrencyUnit.USD, "$" },
+            { CurrencyUnit.JPY, "¥" },
+            { CurrencyUnit.BGN, "лв" },
+            { CurrencyUnit.CZK, "Kč" },
+            { CurrencyUnit.DKK, "kr" },
+            { CurrencyUnit.GBP, "£" },
+            { CurrencyUnit.HUF, "Ft" },
+            { CurrencyUnit.PLN, "zł" },
+            { CurrencyUnit.RON, "lei" },
+            { CurrencyUnit.SEK, "kr" },
+            { CurrencyUnit.CHF, "CHF" },
+            { CurrencyUnit.ISK, "kr" },
+            { CurrencyUnit.NOK, "kr" },
+            { CurrencyUnit.HRK, "kn" },
+            { CurrencyUnit.RUB, "₽" },
+            { CurrencyUnit.TRY, "₺" },
+            { CurrencyUnit.AUD, "$" },
+            { CurrencyUnit.BRL, "R$" },
+            { CurrencyUnit.CAD, "$" },
+            { CurrencyUnit.CNY, "¥" },
+            { CurrencyUnit.HKD, "$" },
+            { CurrencyUnit.IDR, "Rp" },
+            { CurrencyUnit.ILS, "₪" },
+            { CurrencyUnit.INR, "₹" },
+            { CurrencyUnit.KRW, "₩" },
+            { CurrencyUnit.MXN, "$" },
+            { CurrencyUnit.MYR, "RM" },
+            { CurrencyUnit.NZD, "$" },
+            { CurrencyUnit.PHP, "₱" },
+            { CurrencyUnit.SGD, "$" },
+            { CurrencyUnit.THB, "฿" },
+            { CurrencyUnit.ZAR, "R" }
+        };
+
+        public static string GetUnit(CurrencyUnit unit)
+        {
+            return Units[unit];
+        }
+
+        // TODO Currency exchange from memory
+    }
+}
---
+++ FunctionsCore/Enums/Unit/CurrencyUnit.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionsCore.Enums.Unit
+{
+    /// <summary>
+    /// Currencies from FreecurrencyAPI
+    /// </summary>
+    public enum CurrencyUnit
+    {
+        /// <summary>
+        /// Euro
+        /// </summary>
+        EUR,
+
+        /// <summary>
+        /// US Dollar
+        /// </summary>
+        USD,
+
+        /// <summary>
+        /// Japanese Yen
+        /// </summary>
+        JPY,
+
+        /// <summary>
+        /// Bulgarian Lev
+        /// </summary>
+        BGN,
+
+        /// <summary>
+        /// Czech Republic Koruna
+        /// </summary>
+        CZK,
+
+        /// <summary>
+        /// Danish Krone
+        /// </summary>
+        DKK,
+
+        /// <summary>
+        /// British Pound Sterling
+        /// </summary>
+        GBP,
+
+        /// <summary>
+        /// Hungarian Forint
+        /// </summary>
+        HUF,
+
+        /// <summary>
+        /// Polish Zloty
+        /// </summary>
+        PLN,
+
+        /// <summary>
+        /// Romanian Leu
+        /// </summary>
+        RON,
+
+        /// <summary>
+        /// Swedish Krona
+        /// </summary>
+        SEK,
+
+        /// <summary>
+        /// Swiss Franc
+        /// </summary>
+        CHF,
+
+        /// <summary>
+        /// Icelandic Króna
+        /// </summary>
+        ISK,
+
+        /// <summary>
+        /// Norwegian Krone
+        /// </summary>
+        NOK,
+
+        /// <summary>
+        /// Croatian Kuna
+        /// </summary>
+        HRK,
+
+        /// <summary>
+        /// Russian Ruble
+        /// </summary>
+        RUB,
+
+        /// <summary>
+        /// Turkish Lira
+        /// </summary>
+        TRY,
+
+        /// <summary>
+        /// Australian Dollar
+        /// </summary>
+        AUD,
+
+        /// <summary>
+        /// Brazilian Real
+        /// </summary>
+        BRL,
+
+        /// <summary>
+        /// Canadian Dollar
+        /// </summary>
+        CAD,
+
+        /// <summary>
+        /// Chinese Yuan
+        /// </summary>
+        CNY,
+
+        /// <summary>
+        /// Hong Kong Dollar
+        /// </summary>
+        HKD,
+
+        /// <summary>
+        /// Indonesian Rupiah
+        /// </summary>
+        IDR,
+
+        /// <summary>
+        /// Israeli New Sheqel
+        /// </summary>
+        ILS,
+
+        /// <summary>
+        /// Indian Rupee
+        /// </summary>
+        INR,
+
+        /// <summary>
+        /// South Korean Won
+        /// </summary>
+        KRW,
+
+        /// <summary>
+        /// Mexican Peso
+        /// </summary>
+        MXN,
+
+        /// <summary>
+        /// Malaysian Ringgit
+        /// </summary>
+        MYR,
+
+        /// <summary>
+        /// New Zealand Dollar
+        /// </summary>
+        NZD,
+
+        /// <summary>
+        /// Philippine Peso
+        /// </summary>
+        PHP,
+
+        /// <summary>
+        /// Singapore Dollar
+        /// </summary>
+        SGD,
+
+        /// <summary>
+        /// Thai Baht
+        /// </summary>
+        THB,
+
+        /// <summary>
+        /// South African Rand
+        /// </summary>
+        ZAR,
+    }
+}
